e437f9d0af7948bb604275d527d4d1b12e7e2910
Momchil Varbanov <momchil.varbanov97@gmail.com>
2021-08-24 15:23:09 +0300

    Mediator Handlers are now taking care of the http responses.

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Core;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -12,9 +13,9 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<List<Activity>> { }
+        public class Query : IRequest<Result<List<Activity>>> { }
 
-        public class Handler : IRequestHandler<Query, List<Activity>>
+        public class Handler : IRequestHandler<Query, Result<List<Activity>>>
         {
             private readonly DataContext _context;
             private readonly ILogger<List> _logger;
@@ -24,23 +25,9 @@ namespace Application.Activities
                 _logger = logger;
             }
 
-            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken cancellationToken)
             {
-                // try
-                // {
-                //     for (int i = 0; i < 10; i++)
-                //     {
-                //         cancellationToken.ThrowIfCancellationRequested();
-                //         await Task.Delay(1000, cancellationToken);
-                //         _logger.LogInformation($"Task {i} has completed!");
-                //     }
-                // }
-                // catch (Exception ex) when ( ex is TaskCanceledException )
-                // {
-                //     _logger.LogInformation("Task was canceled!");
-                // }
-
-                return await _context.Activities.ToListAsync(cancellationToken);
+                return Result<List<Activity>>.Success(await _context.Activities.ToListAsync(cancellationToken));
             }
         }
     }
