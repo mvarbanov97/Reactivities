eec5fe6d5dc3e690844eb9b5e170c0703f26ce57
Momchil Varbanov <momchil.varbanov97@gmail.com>
2021-08-25 13:26:24 +0300

    Added actions to the Accounts Controller
    - API Controller now handles register request
    - API Controller now returns the current logged in user.
    - Added Register and User DTO's
    - implemented private method that returns UserDto as it is used frequently

---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class RegisterDto
+    {
+        [Required]
+        public string DisplayName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Passowrd { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+    }
+}
