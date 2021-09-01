ee0f4564fffef1cceb5b2bfae4db171d2cb5fc26
Momchil Varbanov <momchil.varbanov97@gmail.com>
2021-09-01 19:04:48 +0300

    Small cosmetic changes
    - border of the followed/following users is bigger
    - activity date formatting fix

--- client-app/src/app/stores/commentStore.ts
+++ client-app/src/app/stores/commentStore.ts
@@ -25,7 +25,7 @@ export default class CommentStore {
             this.hubConnection.on('LoadComments', (comments:ChatComment[]) => {
                 runInAction(() => {
                     comments.forEach(comment => {
-                        comment.createdAt = new Date(comment.createdAt);
+                        comment.createdAt = new Date(comment.createdAt + 'Z');
                     });
                     this.comments = comments
                 })
@@ -33,7 +33,7 @@ export default class CommentStore {
 
             this.hubConnection.on('ReceiveComment', (comment:ChatComment) => {
                 runInAction(() => {
-                    comment.createdAt = new Date(comment.createdAt + 'Z');
+                    comment.createdAt = new Date(comment.createdAt);
                     this.comments.unshift(comment);
                 })
             });
