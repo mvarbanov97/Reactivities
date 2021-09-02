08362cfae93893a9cc844b3e368292f674970802
Momchil Varbanov <momchil.varbanov97@gmail.com>
2021-09-02 15:31:41 +0300

    Updated activity dashboard as it now displaying placeholders while activities are being loaded from API

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -7,6 +7,7 @@ import { useStore } from '../../../app/stores/store';
 import ActivityFilters from './ActivityFilters';
 import ActivityList from './ActivityList';
 import InfiniteScroll from 'react-infinite-scroller';
+import ActivityListItemPlaceholder from './ActivityListItemPlaceholder';
 
 export default observer(function ActivityDashboard() {
    const { activityStore } = useStore();
@@ -22,19 +23,23 @@ export default observer(function ActivityDashboard() {
       if (activityRegistry.size <= 1) loadActivities();
    }, [activityRegistry.size, loadActivities]);
 
-   if (activityStore.loadingInitial && !loadingNext) return <LoadingComponent content='Loading activities...' />;
-
    return (
       <Grid>
          <Grid.Column width='10'>
-            <InfiniteScroll
-               pageStart={0}
-               loadMore={handleGetNext}
-               hasMore={!loadingNext && !!pagination && pagination.currentPage < pagination.totalPages}
-               initialLoad={false}
-            >
-               <ActivityList />
-            </InfiniteScroll>
+            {activityStore.loadingInitial && !loadingNext ? (
+               <>
+                  <ActivityListItemPlaceholder />
+               </>
+            ) : (
+               <InfiniteScroll
+                  pageStart={0}
+                  loadMore={handleGetNext}
+                  hasMore={!loadingNext && !!pagination && pagination.currentPage < pagination.totalPages}
+                  initialLoad={false}
+               >
+                  <ActivityList />
+               </InfiniteScroll>
+            )}
          </Grid.Column>
          <Grid.Column width='6'>
             <ActivityFilters />
