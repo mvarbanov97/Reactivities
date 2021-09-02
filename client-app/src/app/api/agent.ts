aa4a33d9a871ee04ab0caf5bc7f722237d7d8d0b
Momchil Varbanov <momchil.varbanov97@gmail.com>
2021-09-02 17:19:01 +0300

    Client agent now have request for user activitites
    - Added userActivity interface (wrap up the data from the server in the interface)
    - Added state to the profile store that will be responsible for user activities
    - Created method which will load user activities based on username and predicate

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -3,7 +3,7 @@ import { toast } from "react-toastify";
 import { history } from "../..";
 import { Activity, ActivityFormValues } from "../models/activity";
 import { PaginatedResult } from "../models/pagination";
-import { Photo, Profile } from "../models/profile";
+import { Photo, Profile, UserActivtiy } from "../models/profile";
 import { User, UserFormValues } from "../models/user";
 import { store } from "../stores/store";
 
@@ -102,6 +102,7 @@ const Profiles = {
   updateProfile: (profile: Partial<Profile>) => requests.put(`/profiles`, profile),
   updateFollowing: (username: string) => requests.post(`/follow/${username}`, {}),
   listFollowings: (username: string, predicate: string) => requests.get<Profile[]>(`/follow/${username}?predicate=${predicate}`),
+  listActivities: (username: string, predicate: string) => requests.get<UserActivtiy[]>(`/profiles/${username}/activities?predicate=${predicate}`),
 }
 
 const agent = {
