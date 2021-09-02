21a7f9eb35b7b0cb1987be906b5dfb8a566c4b96
Momchil Varbanov <momchil.varbanov97@gmail.com>
2021-09-02 12:48:48 +0300

    Added client-side pagination
    - Created interfaces and classes which are used to store data which is returned or send from/to the API
    - axios interceptors is watching if the response is having pagination headers. if it is it is returning the response data as paginated result.
    - added state to the activityStore which is used for pagination

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -2,6 +2,7 @@ import { format } from 'date-fns';
 import { makeAutoObservable, runInAction } from 'mobx';
 import agent from '../api/agent';
 import { Activity, ActivityFormValues } from '../models/activity';
+import { Pagination, PagingParams } from '../models/pagination';
 import { Profile } from '../models/profile';
 import { store } from './store';
 
@@ -11,9 +12,22 @@ export default class ActivityStore {
    editMode = false;
    loading = false;
    loadingInitial = false;
+   pagination: Pagination | null = null;
+   pagingParams = new PagingParams();
 
    constructor() {
       makeAutoObservable(this);
+   }
+
+   setPagingParams = (pagingParams: PagingParams) => {
+      this.pagingParams = pagingParams;
+   }
+
+   get axiosParams() {
+      const params = new URLSearchParams();
+      params.append('pageNumber', this.pagingParams.pageNumber.toString());
+      params.append('pageSize', this.pagingParams.pageSize.toString());
+      return params;
    }
 
    get activitiesByDate() {
@@ -33,16 +47,21 @@ export default class ActivityStore {
    loadActivities = async () => {
       this.loadingInitial = true;
       try {
-         const activities = await agent.Activities.list();
-         activities.forEach(activity => {
+         const result = await agent.Activities.list(this.axiosParams);
+         result.data.forEach(activity => {
             this.setActivity(activity);
          });
+         this.setPagination(result.pagination);
          this.setLoadingInitial(false);
       } catch (error) {
          console.log(error);
          this.setLoadingInitial(false);
       }
    };
+
+   setPagination = (pagination: Pagination) => {
+      this.pagination = pagination;
+   }
 
    loadActivity = async (id: string) => {
       let activity = this.getActivity(id);
