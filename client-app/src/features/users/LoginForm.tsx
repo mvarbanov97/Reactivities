641d811c736df6dee23ae61b9bd6e7eb1a56c446
Momchil Varbanov <momchil.varbanov97@gmail.com>
2021-09-09 15:29:32 +0300

    Email conformation feature successfully implemented on the client
    - Added request at the agent.ts for verifying endpoints
    - Added Routes at the entry component
    - Created RegisterSuccess/ConfirmEmail components
    - Changed the logic in the register action - now register the account and redirects to register success page

--- client-app/src/features/users/LoginForm.tsx
+++ client-app/src/features/users/LoginForm.tsx
@@ -6,23 +6,23 @@ import MyTextInput from '../../app/common/form/MyTextInput';
 import { useStore } from '../../app/stores/store';
 
 export default observer(function LoginForm() {
-    const {userStore} = useStore();
+    const { userStore } = useStore();
 
     return (
-        <Formik 
-            initialValues={{email: '', password: '', error: null}}
-            onSubmit={(values, {setErrors}) => userStore.login(values).catch(error => 
-                setErrors({error: 'Invalid email or password'}))}
+        <Formik
+            initialValues={{ email: '', password: '', error: null }}
+            onSubmit={(values, { setErrors }) => userStore.login(values).catch(error =>
+                setErrors({ error: error.response.data }))}
         >
-            {({handleSubmit, isSubmitting, errors}) => (
+            {({ handleSubmit, isSubmitting, errors }) => (
                 <Form className='ui form' onSubmit={handleSubmit}>
                     <Header as='h2' content='Login to Reactivities' color='teal' textAlign='center' />
                     <MyTextInput name='email' placeholder='Email' />
                     <MyTextInput name='password' placeholder='Password' type='password' />
                     <ErrorMessage
-                        name='error' 
-                        render={() => 
-                        <Label style={{marginBottom: 10}} basic color='red' content={errors.error} />} 
+                        name='error'
+                        render={() =>
+                            <Label style={{ marginBottom: 10 }} basic color='red' content={errors.error} />}
                     />
                     <Button loading={isSubmitting} positive content='Login' type='submit' fluid />
                 </Form>
